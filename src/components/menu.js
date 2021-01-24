b3f6752e0c81cc1558024c1169ce91a1ae689cc0
Saskia Leinberger <saskia.leinberger@gmail.com>
2021-01-24 21:09:58 +1300

    New styling

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import { Link } from "gatsby"
+import classes from './layout.module.css'
 
 const Menu = () => (
-    <div style={{
-        background: '#f4f4f4',
-        paddingTop: '10px'
-        }}>
-        
-        <ul style={{
-            listStyle: 'none',
-            display: 'flex',
-            justifyContent: 'space-evenly'
-        }}>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='/services'>Services</Link></li>
-            <li><Link to='/blog'>Blog</Link></li>
-        </ul>
-    </div>
+    <nav className={classes.NavBar}>
+        <div className={classes.NavBrand}>Hello</div>
+            <ul className={classes.NavItems}>
+                <Link className={classes.NavItemLink} to='/'><li className={classes.NavItem}>Home</li></Link>
+                <Link className={classes.NavItemLink} to='/about'><li className={classes.NavItem}>About</li></Link>
+                <Link className={classes.NavItemLink} to='/blog'><li className={classes.NavItem}>Blog</li></Link>
+                <Link className={classes.NavItemLink} to='contact'><li className={classes.NavItem}>Contact</li></Link>
+            </ul>
+    </nav>
     )
 
 export default Menu
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,38 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import classes from '../components/Layout.module.css'
+// import Image from "../components/image"
+import avatar from '../images/avataaars.png'
+import githubIcon from '../images/github.svg'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    {/* <h1>Hi people</h1>
       <p>Welcome to my website.</p>
-      <p>This is a blog of a future dev</p>
+      <p>This is a blog of a future dev</p> */}
+    <div className={classes.TextWrapper}>
+      <h2 className={classes.MainTitle}>
+        Hi people, <br />
+        Welcome to my Blog.
+      </h2>
+        <p className={classes.MainDescription}>
+            Lorem ipsum dolor sit...
+        </p>
+          <Link to='#'>
+            <button className={classes.BtnBoost}>Get Started</button>
+      </Link>
+      </div>
+          <div className={classes.ImgWrapper}>
+            <img className={classes.FeatImg} style={{ width: '350px' }} src={avatar} alt='Avatar'/>
+          </div>
+          {/* <div>
+            <Link to="https://github.com/saskialein" target="_blank"><img className={classes.GithubIcon} src={githubIcon} alt='Github icon with link to github profile'/></Link>
+          </div> */}
   </Layout>
 )
 
